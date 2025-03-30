a9e896472bf430c98bcdb2464d7c012b03e00ed8
iamseki <chrisyuri_19@hotmail.com>
2025-03-30 19:15:21 -0300

    feat: github pages deploy

--- src/components/router.rs
+++ src/components/router.rs
@@ -11,3 +11,12 @@ pub enum Route {
     #[route("/favorites")]
     Favorites,
 }
+
+// The server function at the endpoint "static_routes" will be called by the CLI to generate the list of static
+// routes. You must explicitly set the endpoint to `"static_routes"` in the server function attribute instead of
+// the default randomly generated endpoint.
+#[server(endpoint = "static_routes")]
+async fn static_routes() -> Result<Vec<String>, ServerFnError> {
+    // The `Routable` trait has a `static_routes` method that returns all static routes in the enum
+    Ok(Route::static_routes().iter().map(ToString::to_string).collect())
+}
